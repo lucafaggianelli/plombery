537e0f20c0b9ace14c678c152e637b25b97f96e0
Luca Faggianelli <luca.faggianelli@gmail.com>
2023-03-18 16:22:49 +0100

    Read notifications settings from yml file
    close #28

--- mario/__init__.py
+++ mario/__init__.py
@@ -1,4 +1,5 @@
-from typing import List
+import logging
+from pathlib import Path
 
 from .api import app
 from .notifications import NotificationRule, notification_manager
@@ -6,16 +7,40 @@ from .orchestrator import orchestrator
 from .pipeline.pipeline import Task, Pipeline, PipelineRunStatus, Trigger  # noqa F401
 
 
+_logger = logging.getLogger(__name__)
+_logger.setLevel(logging.INFO)
+_logger.addHandler(logging.StreamHandler())
+
+
 class Mario:
+    def __init__(self) -> None:
+        self._load_configuration()
+
+    def _load_configuration(self, config_file_name: str = None):
+        config_file = Path(config_file_name or "mario.config.yml")
+
+        from yaml import load
+
+        try:
+            # if libyaml is installed
+            from yaml import CLoader as Loader
+        except ImportError:
+            from yaml import Loader
+
+        if config_file.exists():
+            _logger.info(f"Loading config from {config_file.absolute()}")
+            with config_file.open("r", encoding="utf-8") as f:
+                self._apply_config(load(f, Loader=Loader))
+
+    def _apply_config(self, config: dict):
+        for notification in config.get("notifications", []):
+            self.add_notification_rule(NotificationRule(**notification))
+
     def register_pipeline(self, pipeline):
         orchestrator.register_pipeline(pipeline)
 
-    def add_notification_rule(
-        self, status: List[PipelineRunStatus], channels: List[str]
-    ):
-        notification_manager.register_rule(
-            NotificationRule(pipeline_status=status, channels=channels)
-        )
+    def add_notification_rule(self, notification: NotificationRule):
+        notification_manager.register_rule(notification)
 
     # Wrap FastAPI ASGI interface so the Mario object
     # can be served directly by uvicorn
