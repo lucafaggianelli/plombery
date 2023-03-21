f18f947ce88a93a3749479f48e520946a678fc3d
Luca Faggianelli <luca.faggianelli@gmail.com>
2023-03-21 16:28:31 +0100

    Add package version number and fix setup.py
    fix #75

---
+++ mario/_version.py
@@ -0,0 +1,3 @@
+# Keep it aligned with version in package.json
+
+__version__ = "0.2.0"
--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
-import io
-import os
+import json
+from pathlib import Path
 from setuptools import find_packages, setup
 
 
-def read(*paths, **kwargs):
+def read(path: Path, encoding: str = None):
     """Read the contents of a text file safely.
-    >>> read("project_name", "VERSION")
+    >>> read(Path("project_name") / "VERSION")
     '0.1.0'
-    >>> read("README.md")
+    >>> read(Path("README.md"))
     ...
     """
 
     content = ""
-    with io.open(
-        os.path.join(os.path.dirname(__file__), *paths),
-        encoding=kwargs.get("encoding", "utf8"),
+    with path.open(
+        mode="r",
+        encoding=encoding or "utf8",
     ) as open_file:
         content = open_file.read().strip()
     return content
 
 
-def read_requirements(path):
+def read_requirements(path: Path):
     return [
         line.strip()
         for line in read(path).split("\n")
@@ -28,16 +28,30 @@ def read_requirements(path):
     ]
 
 
+def read_package_json() -> dict:
+    """Read frontend package.json file, where all the metadata info are stored"""
+
+    package_json = Path(__file__).parent / "frontend" / "package.json"
+
+    with package_json.open("r", encoding="utf-8") as package_file:
+        return json.load(package_file)
+
+
+package_json = read_package_json()
+
+
 setup(
     name="mario",
-    version=read("mario", "version"),
-    description="project_description",
-    url="https://github.com/lucafaggianelli/mario-pype",
-    long_description=read("README.md"),
+    version=package_json["version"],
+    description=package_json["description"],
+    url=package_json["repository"]["url"],
+    long_description=read(Path("README.md")),
     long_description_content_type="text/markdown",
-    author="author_name",
-    packages=find_packages(exclude=["tests", ".github"]),
-    install_requires=read_requirements("requirements.txt"),
-    extras_require={"dev": read_requirements("requirements-dev.txt")},
+    author=package_json["author"]["name"],
+    author_email=package_json["author"]["email"],
+    license=package_json["license"],
+    packages=find_packages(exclude=["tests", "frontend", ".github"]),
+    install_requires=read_requirements(Path("requirements.txt")),
+    extras_require={"dev": read_requirements(Path("requirements-dev.txt"))},
     include_package_data=True,
 )
