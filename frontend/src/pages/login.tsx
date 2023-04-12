9a50fdfca0c388ed02b735eb7db318b272bcf616
Luca Faggianelli <luca.faggianelli@gmail.com>
2023-04-12 18:15:03 +0200

    better login page

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,18 +1,28 @@
-import { Button } from '@tremor/react'
+import { Button, Card, Title } from '@tremor/react'
 
 import { getApiUrl } from '@/repository'
 
 const LoginPage: React.FC = () => {
   return (
     <div className="h-screen flex justify-center items-center bg-slate-200">
-      <Button
-        onClick={() => {
-          location.href = `${getApiUrl()}/login`
-        }}
-        color="indigo"
-      >
-        Login
-      </Button>
+      <Card className="w-auto">
+        <div className="w-12 rounded-full mx-auto mb-4">
+          <img src="/mario-pipe-flower.png" alt="Mario Pype logo" />
+        </div>
+
+        <Title className="mb-6">Welcome to Mario Pype</Title>
+
+        <Button
+          onClick={() => {
+            location.href = `${getApiUrl()}/login`
+          }}
+          size="lg"
+          color="indigo"
+          className="shadow-none w-full"
+        >
+          Login
+        </Button>
+      </Card>
     </div>
   )
 }
